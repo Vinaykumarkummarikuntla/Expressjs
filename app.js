5c2b0924df035ed92d947443745058ac23a6dfa5
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-05-14 17:24:47 +0530

    middleswares and post method

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action = "/products" method = "POST"><input type= "text" name ="title"> <input type= "text" name ="size"><button type = "submit">Add</button></form>'
+  );
   console.log("Starting middleware");
-  next();
 });
 
-app.use((req, res, next) => {
-  console.log("Starting 2second middleware");
+app.post("/products", (req, res, next) => {
+  console.log("products page loaded");
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<html><h1>Welcome to Express.js</h1></html>");
 });
 
